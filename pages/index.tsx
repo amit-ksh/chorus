65c92bc5ab9e5d2bae6dd31bef91ba729f72c112
Amit Kumar Sharma <91947037+amit-2k1@users.noreply.github.com>
2022-03-29 20:08:19 +0530

    added playlist page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home = ({ artists }) => {
       color="green"
       subtitle="profile"
       title={`${user?.firstName} ${user?.lastName}`}
-      description={`${user.playlistsCount} public playlist`}
+      description={`${user?.playlistsCount} public playlist`}
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=1380&userId=&cache=v2"
       roundImage
     >
---
+++ pages/playlist/[id].tsx
@@ -0,0 +1,53 @@
+import GradientLayout from "../../components/gradientLayout";
+import SongsTable from "../../components/songsTable";
+import { validateToken } from "../../lib/auth";
+import prisma from "../../lib/prisma";
+
+const getBGColor = (id) => {
+  const colors = ["red", "green", "orange", "purple", "gray", "teal", "yellow"];
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
+const Playlist = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+  return (
+    <GradientLayout
+      color={color}
+      title={playlist.name}
+      subtitle="Playlist"
+      description={`${playlist.songs.length} songs`}
+      image={`https://picsum.photos/400?random=${playlist.id}`}
+      roundImage={false}
+    >
+      <SongsTable songs={playlist.songs} />
+    </GradientLayout>
+  );
+};
+
+export const getServerSideProps = async ({ query, req }) => {
+  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  const [playlist] = await prisma.playlist.findMany({
+    where: {
+      id: +query.id,
+      userId: id,
+    },
+    include: {
+      songs: {
+        include: {
+          artist: {
+            select: {
+              name: true,
+              id: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return {
+    props: { playlist },
+  };
+};
+
+export default Playlist;
